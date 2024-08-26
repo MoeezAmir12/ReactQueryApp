d90f51e3d99de4e18998ceecb0da7772227dcabe
MOEEZAMIR\Moeez Amir <moeeza.contractor@elementsgs.com>
2024-08-26 17:01:55 +0500

    - updated styling, optimized code and added form

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import { getPostsData,insertPostData } from './APIReducers/PostsAPIReducer';
 import { useQueryClient, useQuery, useMutation} from 'react-query';
 
 function App() {
-  const [title,setTitle] = useState<string>("");
-  const [body,setBody] = useState("");
- 
   const query = useQueryClient();
+  const [formData,setFormData] = useState({
+    title: "",
+    body: ""
+  })
+ const [btnLabel,setBtnLabel] = useState("Add Post");
+  const handleChanges = (event: any) => {
+  setFormData({...formData,[event.target.name]:event.target.value})
+  }
+
   const handleNewPost = async() => {
-    const payload = {
-      title: title,
-      body: body
-    }
-  await mutateAsync(payload);
+  await mutateAsync(formData);
   }
 
   const {mutateAsync} = useMutation({
@@ -25,6 +27,17 @@ function App() {
       query.invalidateQueries({queryKey:['added']})
     }
   })
+
+  const submitHandler = async(e: any) => {
+    e.preventDefault();
+  setBtnLabel("Adding Post...")
+  await handleNewPost();
+  setBtnLabel("Add Post");
+  setFormData({
+    title: "",
+    body:""
+  })
+  }
 
   const {isLoading, data : posts, error, isError} = useQuery({
     queryFn : async() => await getPostsData(),
@@ -45,16 +58,21 @@ function App() {
     <span className='text-center text-red-500 font-bold text-2xl'>React Query VS Use Effect</span>
     </div>
     <div className='w-fit flex flex-col gap-4'>
-      <div className='flex flex-col w-full justify-start gap-3'>
-        <div className='flex flex-col gap-0.5'>
-          <label>Enter Post Tile</label>
-        <input value={title} type='text' name='Title' onChange={(e) => setTitle(e.target.value)} placeholder='Enter Title'/>
+      <div className='flex flex-col w-fit h-fit p-4 border-4 border-indigo-400 rounded-md justify-start'>
+      <form onSubmit={(e) => submitHandler(e)} className='flex flex-col w-fit h-fit gap-5'>
+        <label className='text-center text-indigo-400 font-extrabold'>Add Post Form</label>
+        <div className='flex flex-col gap-1'>
+          <label className='text-indigo-400 font-bold'>Enter Post Tile</label>
+        <input value={formData.title} className='outline-1 outline-indigo-400 p-2 outline focus:outline-2 focus:outline-indigo-600' type='text' name='title' onChange={(e) => handleChanges(e)} required placeholder='Enter Title'/>
         </div>
-        <div className='flex flex-col gap-0.5'>
-          <label>Write Post</label>
-        <textarea cols={4} rows={5} autoFocus value={body} maxLength={80} onChange={(e) => setBody(e.target.value)} placeholder='Enter Post Detail'></textarea>
+        <div className='flex flex-col gap-1'>
+          <label className='text-indigo-400 font-bold'>Write Post</label>
+        <textarea name='body' className='outline-1 outline-indigo-400 p-2 outline focus:outline-2 focus:outline-indigo-600' cols={4} rows={5} autoFocus value={formData.body} maxLength={80} onChange={(e) => handleChanges(e)} placeholder='Enter Post Detail' required></textarea>
         </div>
-        <button type='button' className='w-fit h-fit p-2 text-slate-100 bg-indigo-400' onClick={()=> handleNewPost()} disabled={title?.length === 0 || body?.length === 0}>Add Post</button>
+        <div>
+        <button type='submit' className='w-fit h-fit p-2 text-slate-100 bg-indigo-400 disabled:bg-indigo-200 disabled:pointer-events-none' disabled={btnLabel === "Adding Post..."}>{btnLabel}</button>
+        </div>
+        </form>
       </div>
     <div className='flex flex-col gap-4 mt-10'>
     <label className='text-2xl'>Posts</label>
@@ -64,9 +82,9 @@ function App() {
       body: string;
     })=> {
       return(
-        <div className='flex flex-col gap-2 p-2 w-[30%] bg-slate-300 rounded-md'>
-          <label className='text-lg'>Title: {post?.title}</label>
-          <p className='text-sm bg-emerald-300'>{post?.body}</p>
+        <div className='flex flex-col gap-2 p-2 w-[30%] bg-slate-300 rounded-md border-2 border-indigo-400 text-left'>
+          <label className='text-lg p-1'>Title: {post?.title}</label>
+          <p className='text-sm bg-indigo-300 p-1 text-left'>{post?.body}</p>
           </div>
       )
     })}
